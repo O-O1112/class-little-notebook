ced3a5490a8978ffacf2a25a8a929e4af4db3e19
O-O1112 <liguoguo103@gmail.com>
2026-09-06 19:24:32 +0800

    Remove Google login and add homework duplication

--- public/app-config.js
+++
@@ -1,5 +0,0 @@
-// Public, non-secret site configuration. Google Client IDs are safe to expose here.
-window.CLASS_NOTEBOOK_CONFIG = {
-  // Replace with the Web OAuth client ID from Google Cloud Console.
-  googleClientId: '',
-};
